25fae0af5c0946992aefa4704db0c5b907898b80
Michaela Jones <michaela.jones03@yahoo.com>
2025-10-10 01:50:09 -0700

    updated files and added new files

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
